092c83e9bbec98bfccc6027d4996c730200a1ed0
Voidless_Psycho <mgollapudi104@gmail.com>
2026-06-09 22:54:48 -0700

    Added Explanation to UWGPA

--- scripts.js
+++ scripts.js
@@ -2,48 +2,32 @@ document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector("form");
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-        let totalGradePoints = 0;
+        let pointsUW = 0;
+        let pointsW = 0;
         for (let i = 1; i <= 7; i++){
-            
-            // get the grade value from textbox
-            // get the weighted yes/no from checkbox
-            // if weighted (1.0-5.0)
-            // if not weighted (0.0-4.0)
-            // add to total
             let letterGrade = document.getElementById(`grade${i}`).value.toUpperCase();
             let weightedNot = document.getElementById(`w${i}`).checked;
-            console.log(letterGrade);
-            console.log(weightedNot);
             
             let points;
-            if (weightedNot) {
-                if (["A+","A","A-"].includes(letterGrade)) {
-                    points = 5.0;
-                } else if (["B+", "B", "B-"].includes(letterGrade)) {
-                    points = 4.0;
-                } else if (["C+", "C", "C-"].includes(letterGrade)) {
-                    points = 3.0;
-                } else if (["D+", "D", "D-"].includes(letterGrade)) {
-                    points = 2.0;
-                } else if (["F+", "F", "F-"].includes(letterGrade)) {
-                    points = 1.0;
-                }
-            } else {
-                if (["A+","A","A-"].includes(letterGrade)) {
-                    points = 4.0;
-                } else if (["B+", "B", "B-"].includes(letterGrade)) {
-                    points = 3.0;
-                } else if (["C+", "C", "C-"].includes(letterGrade)) {
-                    points = 2.0;
-                } else if (["D+", "D", "D-"].includes(letterGrade)) {
-                    points = 1.0;
-                } else if (["F+", "F", "F-"].includes(letterGrade)) {
-                    points = 0.0;
-                }
+            if (["A+","A","A-"].includes(letterGrade)) {
+                points = 4.0;
+            } else if (["B+", "B", "B-"].includes(letterGrade)) {
+                points = 3.0;
+            } else if (["C+", "C", "C-"].includes(letterGrade)) {
+                points = 2.0;
+            } else if (["D+", "D", "D-"].includes(letterGrade)) {
+                points = 1.0;
+            } else if (["F+", "F", "F-"].includes(letterGrade)) {
+                points = 0.0;
             }
-            totalGradePoints += points;
+            
+            pointsUW += points;
+            if (weightedNot) { points += 1.0 };
+            pointsW += points;
         }
-        totalGradePoints = (totalGradePoints /= 7).toFixed(2);
-        document.getElementById("wgpa").innerHTML = totalGradePoints;
+        let uw = (pointsUW /= 7).toFixed(2);
+        let w = (pointsW /= 7).toFixed(2);
+        document.getElementById("wgpa").innerHTML = w;
+        document.getElementById("uwgpa").innerHTML = uw;
     });
 });
